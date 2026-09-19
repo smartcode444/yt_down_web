182641075b0c68d0a5128ecdf5d8299d742176ce
smartcode444 <smartcoder619@gmail.com>
2026-09-19 13:18:46 +0100

    Add download functionality to homepage with js

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+# from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -9,3 +9,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+def read_root(request: Request):
+    """read_root"""
+    return  templates.TemplateResponse(
+        request=request, name="index.html", context={"title": "home_page"}
+    )
